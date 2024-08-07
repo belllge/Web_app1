04b556ada81e355b4365e51e3d933faadcf32899
belje <beletehagos@gmail.com>
2024-08-07 14:02:27 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Welcome to the Hamster Kombat Key Generator Bot! Use /getcode <game_number> <key_count> to generate a key. example if you want two keys for 'riding extreme 3d' use /getcode 1 2')
+    await update.message.reply_text("Welcome to the Hamster Kombat Key Generator Bot! Use /getcode <game_number> <key_count> to generate a key. example if you want two keys for 'riding extreme 3d' use /getcode 1 2")
 
 async def get_code(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Your existing code to generate and return the key
